e4a61277ec8aed3aaba875e19e20b4f5a9cfdafa
unknown <kky6108@naver.com>
2025-04-15 16:51:10 +0900

    � remove lingering test submodule (clean)

--- src/test/src/config-overrides.js
+++
@@ -1,8 +0,0 @@
-module.exports = function override(config, env) {
-    config.resolve.fallback = {
-      http: require.resolve("stream-http"),
-      https: require.resolve("https-browserify"),
-      stream: require.resolve("stream-browserify"),
-    };
-    return config;
-  };
