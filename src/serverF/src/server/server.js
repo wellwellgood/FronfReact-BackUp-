4055fe4b19e44925ec2a8f054ffd6a1c41e3e925
wellwellgood <kky6108@naver.com>
2025-04-16 13:11:32 +0900

    Update server.js

--- src/serverF/src/server/server.js
+++ src/serverF/src/server/server.js
@@ -7,7 +7,7 @@ const jwt = require("jsonwebtoken");
 const pool = require("../DB.js");
 const http = require("http");
 const socket = require("./socket.js");
-const userRoutes = require("../routes/userRoutes.js");
+const userRoutes = require("./routes/userRoutes.js");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
