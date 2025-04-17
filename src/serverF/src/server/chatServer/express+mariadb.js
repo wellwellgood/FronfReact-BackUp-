f4a1ff6cf86292527e7b9ca565f0b0e12d75aad9
wellwellgood <kky6108@naver.com>
2025-04-17 09:08:33 +0900

    Update express+mariadb.js

--- src/serverF/src/server/chatServer/express+mariadb.js
+++ src/serverF/src/server/chatServer/express+mariadb.js
@@ -1,15 +1,18 @@
+require('dotenv').config(); // 꼭 맨 위에 추가
+
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "3333",
-  database: "chat", // 실제 DB 이름
-  port: 3307,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
   connectionLimit: 20,
 });
 
 module.exports = pool;
+
 (async () => {
   let conn;
   try {
