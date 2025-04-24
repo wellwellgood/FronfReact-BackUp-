454d77f6c383b9ac311a47d62600e8d0027ce312
wellwellgood <kky6108@naver.com>
2025-04-24 16:15:58 +0900

    Update membership.js

--- src/membership.js
+++ src/membership.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import styles from "./serverF/chatServer/css/membership.module.css";
+import styles from "./membership.module.css";
 
 export default function Membership() {
   const navigate = useNavigate();
